55397ffb96dd2a73f17e6fda72c47570418e21aa
Aaron Marr <aaronmarruk@gmail.com>
2014-10-08 16:10:29 +0100

    Develop index page.

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,1 +1,8 @@
 // Write JS here
+$(document).ready(function(){
+	$('.feedback').on('click', '.feedback__show-form-btn', function(){
+		console.log("toggling");
+		console.log(this);
+		$('.feedback__form').toggle();
+	});
+});
